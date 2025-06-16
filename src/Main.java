b505f21bc9bd1f3a119cb9c7f222067cd63a5885
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-16 14:36:34 +0900

    OuterInMemory

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+class Outer {
+    static class StaticInner {
+        void hello() {
+            System.out.println("Hi!");
+        }
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+            Outer.StaticInner inner = new Outer.StaticInner();
+            inner.hello();
+    }
+}
